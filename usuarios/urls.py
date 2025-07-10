c47de045086003d6de9e143eb635aee0446e8177
Cristian D Biancotti <cdbiancotti@gmail.com>
2025-07-10 20:48:03 -0300

    Finiquiteamos clase 23 - agregamos puntos para entrega final

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,6 +6,7 @@ app_name = 'usuarios'
 
 urlpatterns = [
     path('login/', views.login, name='login'),
+    path('registro/', views.registro, name='registro'),
     path('logout/', LogoutView.as_view(template_name='usuarios/logout.html'), name='logout')
 ]
  
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login as django_login
+from usuarios.forms import FormularioRegistro
 
 def login(request):
     
@@ -15,3 +16,16 @@ def login(request):
         formulario = AuthenticationForm()
     
     return render(request, 'usuarios/login.html', {'formulario': formulario})
+
+def registro(request):
+    
+    if request.method == 'POST':
+        # formulario = UserCreationForm(request.POST)
+        formulario = FormularioRegistro(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('usuarios:login')
+    else:
+        # formulario = UserCreationForm()
+        formulario = FormularioRegistro()
+    return render(request, 'usuarios/registro.html', {'formulario': formulario})
